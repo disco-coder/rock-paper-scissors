37c594129f13a9ecf2230fb4ad819133e7590011
disco-coder <1koblizek1@gmail.com>
2019-10-23 15:26:19 +0200

    Move JS to separate file

---
+++ app.js
@@ -0,0 +1,209 @@
+// Variables for player scores
+let playerWins = 0;
+let computerWins = 0;
+const playerRoll = document.querySelector("#playerRoll");
+const computerRoll = document.querySelector("#computerRoll");
+const roundResult = document.querySelector("#roundResult");
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
+const announcer = document.querySelector("#announcer");
+
+
+// Buttons is a node list (looks and acts similarly to an array)
+const buttons = document.querySelectorAll("button");
+
+// Use .forEach to iterate through each button in the node list
+buttons.forEach((button) => {
+  
+    // Add "click" event listener to each button
+    button.addEventListener("click", (e) => {
+    playRound(button.id, computerPlay())
+    })
+})
+
+// Randomly choose what computer plays
+function computerPlay() {
+    let randomRoll = Math.random()
+    if (randomRoll < (1/3)) {
+        return "rock";
+    } else if (randomRoll > (1/3) && randomRoll < (2 * 1/3)) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+// Plays one round of the game
+function playRound(playerSelection, computerSelection) {
+    if (playerWins === 0 && computerWins === 0) {
+        announcer.textContent = "";
+    }
+  
+    // Compare rolls, declare winner and update player scores
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") {
+            playerRoll.textContent = `You played ${playerSelection}`;
+            computerRoll.textContent = "Computer plays rock";
+            roundResult.textContent = "It's a draw!";
+            playerScore.textContent = `Player ${playerWins}  -  `;
+            computerScore.textContent = `${computerWins} Computer`;
+            if (playerWins === 5) {
+                announcer.textContent = "YOU WIN THIS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            if (computerWins === 5) {
+                announcer.textContent = "COMPUTER WINS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            return
+        } else if (computerSelection == "paper") {
+            playerRoll.textContent = `You played ${playerSelection}`;
+            computerRoll.textContent = `Computer plays ${computerSelection}`;
+            roundResult.textContent = "Paper beats rock, you lose!";
+            ++computerWins;
+            playerScore.textContent = `Player ${playerWins}  -  `;
+            computerScore.textContent = `${computerWins} Computer`;
+            if (playerWins === 5) {
+                announcer.textContent = "YOU WIN THIS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            if (computerWins === 5) {
+                announcer.textContent = "COMPUTER WINS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            return
+        } else {
+            playerRoll.textContent = `You played ${playerSelection}`;
+            computerRoll.textContent = `Computer plays ${computerSelection}`;
+            roundResult.textContent = "Rock beats scissors, you win!";
+            ++playerWins;
+            playerScore.textContent = `Player ${playerWins}  -  `;
+            computerScore.textContent = `${computerWins} Computer`;
+            if (playerWins === 5) {
+                announcer.textContent = "YOU WIN THIS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            if (computerWins === 5) {
+                announcer.textContent = "COMPUTER WINS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            return
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "paper") {
+            playerRoll.textContent = `You played ${playerSelection}`;
+            computerRoll.textContent = `Computer plays ${computerSelection}`;
+            roundResult.textContent = "It's a draw!";
+            playerScore.textContent = `Player ${playerWins}  -  `;
+            computerScore.textContent = `${computerWins} Computer`;
+            if (playerWins === 5) {
+                announcer.textContent = "YOU WIN THIS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            if (computerWins === 5) {
+                announcer.textContent = "COMPUTER WINS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            return
+        } else if (computerSelection == "rock") {
+            playerRoll.textContent = `You played ${playerSelection}`;
+            computerRoll.textContent = `Computer plays ${computerSelection}`;
+            roundResult.textContent = "Paper beats rock, you win!";
+            ++playerWins;
+            playerScore.textContent = `Player ${playerWins}  -  `;
+            computerScore.textContent = `${computerWins} Computer`;
+            if (playerWins === 5) {
+                announcer.textContent = "YOU WIN THIS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            if (computerWins === 5) {
+                announcer.textContent = "COMPUTER WINS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            return
+        } else {
+            playerRoll.textContent = `You played ${playerSelection}`;
+            computerRoll.textContent = `Computer plays ${computerSelection}`;
+            roundResult.textContent = "Scissors beat paper, you lose!";
+            ++computerWins;
+            playerScore.textContent = `Player ${playerWins}  -  `;
+            computerScore.textContent = `${computerWins} Computer`;
+            if (playerWins === 5) {
+                announcer.textContent = "YOU WIN THIS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            if (computerWins === 5) {
+                announcer.textContent = "COMPUTER WINS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            return
+        }
+    } else {
+        if (computerSelection == "scissors") {
+            playerRoll.textContent = `You played ${playerSelection}`;
+            computerRoll.textContent = `Computer plays ${computerSelection}`;
+            roundResult.textContent = "It's a draw!";
+            playerScore.textContent = `Player ${playerWins}  -  `;
+            computerScore.textContent = `${computerWins} Computer`;
+            if (playerWins === 5) {
+                announcer.textContent = "YOU WIN THIS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            if (computerWins === 5) {
+                announcer.textContent = "COMPUTER WINS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            return
+        } else if (computerSelection == "paper") {
+            playerRoll.textContent = `You played ${playerSelection}`;
+            computerRoll.textContent = `Computer plays ${computerSelection}`;
+            roundResult.textContent = "Scissors beat paper, you win!";
+            ++playerWins;
+            playerScore.textContent = `Player ${playerWins}  -  `;
+            computerScore.textContent = `${computerWins} Computer`;
+            if (playerWins === 5) {
+                announcer.textContent = "YOU WIN THIS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            if (computerWins === 5) {
+                announcer.textContent = "COMPUTER WINS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            return
+        } else {
+            playerRoll.textContent = `You played ${playerSelection}`;
+            computerRoll.textContent = `Computer plays ${computerSelection}`;
+            roundResult.textContent = "Rock beats scissors, you lose!";
+            ++computerWins;
+            playerScore.textContent = `Player ${playerWins}  -  `;
+            computerScore.textContent = `${computerWins} Computer`;
+            if (playerWins === 5) {
+                announcer.textContent = "YOU WIN THIS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            if (computerWins === 5) {
+                announcer.textContent = "COMPUTER WINS GAME";
+                computerWins = 0;
+                playerWins = 0;
+            }
+            return
+        }
+    }    
+}
